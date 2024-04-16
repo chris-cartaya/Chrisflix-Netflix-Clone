def51c33099100a8a2c8f5196c2f3a8d6eec722d
Chris Cartaya <chriscartaya@gmail.com>
2024-04-15 20:33:39 -0400

    connect to database using config file

--- includes/config.php
+++ includes/config.php
@@ -7,4 +7,18 @@ ob_start(); // Turns on output buffering.
 session_start();
 
 date_default_timezone_set("America/New_York");
+
+
+try {
+    $dsn = "mysql:dbname=chrisflix;host=localhost";
+    $db_user = "root";
+    $db_password = "";
+
+    $con = new PDO($dsn, $db_user, $db_password);
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+} catch(PDOException $e) {
+    exit("Connection failed: {$e->getMessage()}");
+}
+
 ?>
