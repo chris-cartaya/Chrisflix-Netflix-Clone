93e5e7ab270a6429bf70eb0b7805ae800532c953
Chris Cartaya <chriscartaya@gmail.com>
2024-04-15 22:12:26 -0400

    add constant variables for min chars and max chars

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -1,7 +1,18 @@
 <?php
 class Constants {
-    public static $firstNameCharacters = "Your first name must be between 2 and 25 characters";
+    const NAME_MIN_CHARS = 2;
+    const NAME_MAX_CHARS = 25;
 
-    public static $lastNameCharacters = "Your last name must be between 2 and 25 characters";
+    const USERNAME_MIN_CHARS = 5;
+    const USERNAME_MAX_CHARS = 25;
+
+    public static $firstNameCharacters = "Your first name must be between " . self::NAME_MIN_CHARS . " and " . 
+    self::NAME_MAX_CHARS . " characters";
+
+    public static $lastNameCharacters = "Your last name must be between " . self::NAME_MIN_CHARS . " and " . 
+    self::NAME_MAX_CHARS . " characters";
+
+    public static $usernameCharacters = "Username must be between " . self::USERNAME_MIN_CHARS . " and " . 
+    self::USERNAME_MAX_CHARS . " characters";
 }
 ?>
