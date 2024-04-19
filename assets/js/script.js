ae9f80f502ed47b19ad71711087096c1b6d5755e
Chris Cartaya <chriscartaya@gmail.com>
2024-04-19 19:36:06 -0400

    change volume icon to mute and unmute

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 function volumeToggle(button) {
   const muted = $(".previewVideo").prop("muted");
   $(".previewVideo").prop("muted", !muted);
+
+  // Remove mute icon and add volume-up icon
+  $(button).find("i").toggleClass("fa-volume-xmark");
+  $(button).find("i").toggleClass("fa-volume-high");
 }
