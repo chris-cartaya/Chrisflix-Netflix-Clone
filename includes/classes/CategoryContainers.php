3d81aaddb304f64af28fe6cd51d4d5c8c1cf39be
Chris Cartaya <chriscartaya@gmail.com>
2024-04-19 20:54:41 -0400

    show all categories on index page

--- includes/classes/CategoryContainers.php
+++ includes/classes/CategoryContainers.php
@@ -18,18 +18,38 @@ class CategoryContainers {
         $this->username = $username;
     }
 
-    public function showAllCategories() {
+    public function showAllCategories(): string {
 
         $stmt = $this->con->prepare("SELECT * FROM CATEGORIES");
         $stmt->execute();
 
         $html = "<div class='previewCategories'>";
 
+        // $row will continue iterating for each row from the query.
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            $html .= $row["name"] . "<br>";
+            $html .= $this->getCategoryHTML($row, null, true, true);
         }
 
         return $html . "</div>";
+    }
+
+    // This function will get all the entities.
+    // $sqlData will contain each row of the Categories table in the database
+    // and $sqlData will contain id and category name.
+    // $title will be the title of the category. If null, we use the category 
+    // name. If we do specify a title, then we will use it in place of where 
+    // the category name would go.
+    // $tvShows and $movies are both booleans (T/F) on whether or not we want 
+    // to include tv shows and/or movies from the database. Home page will want
+    // both tv shows and movies, and there will be separate 'tabs' for movies 
+    // and tv shows.
+    private function getCategoryHTML(
+        $sqlData, ?string $title, bool $tvShows, bool $movies
+    ): string {
+        $categoryID = $sqlData["id"];
+        $title = $title == null ? $sqlData["name"] : $title;
+
+        return $title . "<br>";
     }
 
 }
