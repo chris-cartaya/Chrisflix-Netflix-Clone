66ad75be4f49651128b691947d8ae5e6f315ec89
Chris Cartaya <chriscartaya@gmail.com>
2024-04-21 04:55:36 -0400

    create search page html and style it

---
+++ search.php
@@ -0,0 +1,8 @@
+<?php
+require_once("includes/header.php");
+?>
+<div class="textBoxContainer">
+    <input type="text" class="searchInput" placeholder="Search for something">
+</div>
+
+<div class="results"></div>
